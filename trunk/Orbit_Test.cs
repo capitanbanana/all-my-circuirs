a64f94acfe76b85b55e02ea41793c1b88445c261
Andrew.Kostousov <Andrew.Kostousov@0a633f90-62df-11de-b47b-210fc227597e>
2009-06-29 15:32:50 +0000

    Формула вычисления площади сектора эллипса теперь с зависимостью только от угла

--- trunk/Orbit_Test.cs
+++ trunk/Orbit_Test.cs
@@ -1,13 +1,35 @@
-using Xunit;
+using System;
+using NUnit.Framework;
 
 namespace ifpfc.Testing
 {
+	[TestFixture]
 	public class Orbit_Test
 	{
-		[Fact]
-		public void S()
+		private const double Eps = 1e-5;
+		
+		[Test]
+		public void CircularSector()
 		{
-			
+			var o = new Orbit {SemiMajorAxis = 1, SemiMinorAxis = 1, TransformAngle = 0};
+			AssertAreClose(o.S(1, 0), Math.PI / 2);
+			AssertAreClose(o.S(1, Math.PI), 0);
+			AssertAreClose(o.S(1, -Math.PI/2), 3*Math.PI/4);
+			AssertAreClose(o.S(1, -Math.PI), Math.PI);
+		}
+
+		[Test]
+		public void EllipticSector()
+		{
+			var o = new Orbit { SemiMajorAxis = 2, SemiMinorAxis = 1, TransformAngle = 0 };
+			AssertAreClose(o.S(Math.PI), 0);
+			AssertAreClose(o.S(0), Math.PI);
+			AssertAreClose(o.S(1, -Math.PI), 2 * Math.PI);
+		}
+
+		private static void AssertAreClose(double a1, double a2)
+		{
+			Assert.Less(Math.Abs(a1 - a2), Eps);
 		}
 	}
 }
