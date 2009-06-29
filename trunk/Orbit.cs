c6e535c6d76fc91fd19c9e669a08cc69a75192bf
Andrew.Kostousov <Andrew.Kostousov@0a633f90-62df-11de-b47b-210fc227597e>
2009-06-29 14:56:00 +0000

    Формула вычисления площади сектора эллипса.

--- trunk/Orbit.cs
+++ trunk/Orbit.cs
@@ -27,6 +27,23 @@ namespace ifpfc
 				double e = Math.Sqrt(1 - ba * ba);
 				return SemiMajorAxis * (1 - e);
 			}
-		}	
+		}
+
+		public double a { get { return SemiMajorAxis; } }
+		public double b { get { return SemiMinorAxis; } }
+		public double c { get { return Math.Sqrt(a * a - b * b); } }
+		public double e { get { return c / a; } }
+
+		//phi in [-pi, pi]
+		public double S(double ro, double phi)
+		{
+			double s0 = Math.PI * a * b;
+			if (phi < 0) return s0 - S(ro, -phi);
+
+			double xa = (ro * Math.Cos(phi) - c) / a;
+			double s1 = (s0 - ro * ro * Math.Sin(2 * phi)) / 4;
+			double s = s1 + (xa * Math.Sqrt(1 - xa * xa) + a * a * Math.Asin(xa)) * b / (2 * a);
+			return s;
+		}
 	}
 }
